0cf6caf614710ddd4e5bedf7fe00fe173f1bb40b
Matvey <realsmych@gmail.com>
2025-06-29 00:13:29 +0100

    Fix Junit BOM

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
 
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
-    testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.platform.launcher)
 
     testImplementation(libs.assertj.core)
 }
